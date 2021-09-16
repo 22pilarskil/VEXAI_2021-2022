5006ff37acc6200e6387e65c8c7fe1cc768ee6ff
Sahm Rahman <58946341+SahmRahman@users.noreply.github.com>
2021-09-16 16:33:58 -0400

    Create Constants.h

---
+++ Odometry/src/Constants.h
@@ -0,0 +1,8 @@
+class Constants
+{
+  double dwBaseWidth;
+  double dwTurnRadius; // distance from deadwheels to center of rotation/robot
+  double dwDiameter = 2.75; // in inches
+  double dwGearing;
+  double dwTicksPerRev;
+}
