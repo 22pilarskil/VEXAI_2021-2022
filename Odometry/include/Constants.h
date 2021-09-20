46d130f5fb811ab1af1c75da69545d71a9cf23a5
Sahm Rahman <58946341+SahmRahman@users.noreply.github.com>
2021-09-20 15:37:48 -0400

    Update Constants.h

--- Odometry/include/Constants.h
+++ Odometry/include/Constants.h
@@ -1,8 +1,8 @@
 class Constants
 {
-  double axialToCenterDist; //distance between axial dead wheels and center, will need 2x for the full width
-  double lateralToCenterDist; // distance from lateral dead wheels and center
-  double dwDiameter = 2.75; // in inches
+  static double axialToCenterDist; //distance between axial dead wheels and center, will need 2x for the full width
+  static double lateralToCenterDist; // distance from lateral dead wheels and center
+  static double dwDiameter = 2.75; // in inches
 //   double dwGearing;
-  double dwTicksPerRev = 90; //not sure if we'll need this
+  static double dwTicksPerRev = 90; //not sure if we'll need this
 }
