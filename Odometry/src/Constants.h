9bebe77784d9be0092efb3a4d61c24d727c9c604
Sahm Rahman <58946341+SahmRahman@users.noreply.github.com>
2021-09-20 15:27:17 -0400

    Update Constants.h

--- Odometry/src/Constants.h
+++ Odometry/src/Constants.h
@@ -1,8 +1,8 @@
 class Constants
 {
-  double dwBaseWidth;
-  double dwTurnRadius; // distance from deadwheels to center of rotation/robot
+  double axialToCenterDist; //distance between axial dead wheels and center, will need 2x for the full width
+  double lateralToCenterDist; // distance from lateral dead wheels and center
   double dwDiameter = 2.75; // in inches
-  double dwGearing;
-  double dwTicksPerRev;
+//   double dwGearing;
+  double dwTicksPerRev = 90; //not sure if we'll need this
 }
