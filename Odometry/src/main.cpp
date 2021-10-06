abb07fa2fb38f2a26191380439c1ea8360e06b07
Sahm Rahman <58946341+SahmRahman@users.noreply.github.com>
2021-10-06 13:41:07 -0400

    update main.cpp
    pretty much just changed for the sake of convention
    the container holding the x, y and heading variables is now in a struct rather than a vector

--- Odometry/src/main.cpp
+++ Odometry/src/main.cpp
@@ -1,8 +1,3 @@
-#include "main.h"
-#include <math.h>
-#include <vector>
-#include "mathutils.h"
-#include "Constants.h"
 #include <vector>
 #include <iostream>
 #include <chrono>
@@ -12,21 +7,25 @@
 
 using namespace std;
 
-void update();
-vector<double> get_pos_estimate();
-void setPose(vector<double> _pos);
+//void update();
+void get_pos_estimate();
+void setPose(struct Position _pos);
 double encoder_to_distance(double ticks);
+
+struct Position
+{
+  double x;
+  double y;
+  double heading; 
+};
+Position pos;
 
 int main()
 {
-  vector<double> pos_vector = get_pos_estimate();
-  cout << "X:" << pos_vector[0] << "\nY:" << pos_vector[1] << "\nHeading:" << pos_vector[2];
+  get_pos_estimate();
+  cout << "X:" << pos.x << "\nY:" << pos.y << "\nHeading:" << pos.heading;
 }
 
-vector<double> pos = {0, 0, 0};
-double x = 0;
-double y = 0;
-double heading = 0;
 double v_x = 0;
 double v_y = 0;
 double v_angular = 0;
@@ -37,18 +36,18 @@ double elPosLast = 0;
 double ebPosLast = 0;
 auto time = chrono::system_clock::now();
 
-void update() {
-	pos = get_pos_estimate();
-	x = pos[0];
-	y = pos[1];
-	heading = pos[2];
-}
+// void update() {
+// 	get_pos_estimate();
+// 	x = pos[0];
+// 	y = pos[1];
+// 	heading = pos[2];
+// }
 
-vector<double> get_pos_estimate() {
-	double prevX = x;
-	double prevY = y;
-	double prevHeading = heading;
-	auto prevTime = *time;
+void get_pos_estimate() {
+	double prevX = pos.x;
+	double prevY = pos.y;
+	double prevHeading = pos.heading;
+	auto prevTime = time;
   time = chrono::system_clock::now();
   chrono::duration<double> elapsed_time = *time - *prevTime;
   cout << elapsed_time.count() << endl;
@@ -76,7 +75,7 @@ vector<double> get_pos_estimate() {
 	// Log.d(TAG, "dx: " + dx);
 	// Log.d(TAG, "dy: " + dy);
 
-	heading = mathutils::normalize(heading+dHeading);
+	pos.heading = mathutils::normalize(pos.heading+dHeading);
 
 	double s,c;
 	if (mathutils::equals(dHeading,0)) {
@@ -90,11 +89,11 @@ vector<double> get_pos_estimate() {
 	double dfX = dx*s-dy*c; //finds local delta x and y
 	double dfY = dx*c+dy*s;
 
-	double dxp = dfX*cos(heading)-dfY*sin(heading);//accounts for change in local deltas into the global deltas
-	double dyp = dfX*sin(heading)+dfY*cos(heading);
+	double dxp = dfX*cos(pos.heading)-dfY*sin(pos.heading);//accounts for change in local deltas into the global deltas
+	double dyp = dfX*sin(pos.heading)+dfY*cos(pos.heading);
 
-	x += dxp;
-	y += dyp;
+	pos.x += dxp;
+	pos.y += dyp;
 
 	orientation += dHeading;
 	rotation = orientation/(2*M_PI);
@@ -119,16 +118,14 @@ vector<double> get_pos_estimate() {
 	erPosLast = erPos;
 	elPosLast = elPos;
 	ebPosLast = ebPos;
-
-	return vector<double> {x,y,heading};
 }
 
-void setPose(vector<double> _pos) {
+void setPose(struct Position _pos) {
 	pos = _pos;
-	x = pos[0];
-	y = pos[1];
-	heading = pos[2];
-	orientation = pos[2];
+	pos.x = _pos.x;
+	pos.y = _pos.y;
+	pos.heading = _pos.heading;
+	orientation = _pos.heading;
 	rotation = 0;
 	erPosLast = 0;
 	elPosLast = 0;
@@ -139,82 +136,4 @@ double encoder_to_distance(double ticks) {
 	double circumference = M_PI*Constants::dwDiameter;
 	double distance = circumference * (ticks/Constants::dwTicksPerRev);
 	return distance;
-}
-
-/**
- * A callback function for LLEMU's center button.
- *
- * When this callback is fired, it will toggle line 2 of the LCD text between
- * "I was pressed!" and nothing.
- */
-void on_center_button() {
-	static bool pressed = false;
-	pressed = !pressed;
-	if (pressed) {
-		pros::lcd::set_text(2, "I was pressed!");
-	} else {
-		pros::lcd::clear_line(2);
-	}
-}
-
-/**
- * Runs while the robot is in the disabled state of Field Management System or
- * the VEX Competition Switch, following either autonomous or opcontrol. When
- * the robot is enabled, this task will exit.
- */
-void disabled() {}
-
-/**
- * Runs after initialize(), and before autonomous when connected to the Field
- * Management System or the VEX Competition Switch. This is intended for
- * competition-specific initialization routines, such as an autonomous selector
- * on the LCD.
- *
- * This task will exit when the robot is enabled and autonomous or opcontrol
- * starts.
- */
-void competition_initialize() {}
-
-/**
- * Runs the user autonomous code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the autonomous
- * mode. Alternatively, this function may be called in initialize or opcontrol
- * for non-competition testing purposes.
- *
- * If the robot is disabled or communications is lost, the autonomous task
- * will be stopped. Re-enabling the robot will restart the task, not re-start it
- * from where it left off.
- */
-void autonomous() {}
-
-/**
- * Runs the operator control code. This function will be started in its own task
- * with the default priority and stack size whenever the robot is enabled via
- * the Field Management System or the VEX Competition Switch in the operator
- * control mode.
- *
- * If no competition control is connected, this function will run immediately
- * following initialize().
- *
- * If the robot is disabled or communications is lost, the
- * operator control task will be stopped. Re-enabling the robot will restart the
- * task, not resume it from where it left off.
- */
-void opcontrol() {
-	pros::Controller master(pros::E_CONTROLLER_MASTER);
-	pros::Motor left_mtr(1);
-	pros::Motor right_mtr(2);
-
-	while (true) {
-		pros::lcd::print(0, "%d %d %d", (pros::lcd::read_buttons() & LCD_BTN_LEFT) >> 2,
-		                 (pros::lcd::read_buttons() & LCD_BTN_CENTER) >> 1,
-		                 (pros::lcd::read_buttons() & LCD_BTN_RIGHT) >> 0);
-		int left = master.get_analog(ANALOG_LEFT_Y);
-		int right = master.get_analog(ANALOG_RIGHT_Y);
-
-		left_mtr = left;
-		right_mtr = right;
-		pros::delay(20);
-	}
 }
