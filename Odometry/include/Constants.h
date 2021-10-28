44ac7fafe6ac04bddf3d83eea3ab22cc5cf60658
Sahm Rahman <58946341+SahmRahman@users.noreply.github.com>
2021-10-28 15:43:36 -0400

    Update Constants.h
    uhhhh messed up the measurement, these are good now

--- Odometry/include/Constants.h
+++ Odometry/include/Constants.h
@@ -1,6 +1,6 @@
 namespace Constants {
-  constexpr double axialToCenterDist = 1.0; //distance between axial dead wheels and center, will need 2x for the full width
-  constexpr double lateralToCenterDist = 3.1875; // distance from lateral dead wheels and center
+  constexpr double axialToCenterDist = 2.875; //distance between axial dead wheels and center, will need 2x for the full width
+  constexpr double lateralToCenterDist = 5.0; // distance from lateral dead wheels and center
   constexpr double dwDiameter = 2.75; // in inches
   // double dwGearing; don't know if we'll need this
   constexpr double dwTicksPerRev = 90; //not sure if we'll need this
