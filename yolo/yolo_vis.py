f0e94629d6025bb6a2acea7d85b27f536b99477d
22seegerb <43051577+22seegerb@users.noreply.github.com>
2021-11-01 16:43:14 -0400

    Fixed Saaz and David's code. Color + Angle + Distance is working
    WORKING:
    - Angle
    - Color
    - Distance (Including discerning between far and close mogos with 0 distance)
    - Finding nearest mogo
    - Sending mogo data to move function
    - ReturnData function to return all mogos, close mogos and filter by color / mogo / ring / ect.
    
    TODO: 
    - Fix turning it's mega broke rn. Whenever the code lags the bot turns like 360 degrees. 
    - Make color not use colorsys
    - Make return_data(find="close") care about mogo colors and exclude rings

--- yolo/yolo_vis.py
+++ yolo/yolo_vis.py
@@ -1,3 +1,4 @@
+
 import pyrealsense2 as rs
 import numpy as np
 import cv2
@@ -10,9 +11,10 @@ import time
 import colorsys
 PATH = "best.pt"
 do_depth_ring = False
+from serial_test import Coms
 
 
-def return_data(mogos, find="all", colors=[-1,0,1]):
+def return_data(mogos, find="all", colors=[-1,0,1], close_thresh=200):
     # Takes in data in the order: [det:[x,y,x,y,dist,color (-1 = red, 0 = yellow, 1 = blue)], det[], .., det[]]
     if find=="all":
         if not colors == [-1,0,1]:
@@ -21,6 +23,24 @@ def return_data(mogos, find="all", colors=[-1,0,1]):
                     del mogos[i]
         return mogos
     if find=="close":
+        mogos[mogos != mogos] = 0 #set all nans to 0
+        det_0 = mogos[mogos[:,4] == 0] #all zeros
+        det_no0 = mogos[mogos[:,4] != 0] #all non-zeros
+
+        if int(det_0.shape[0])>0:  #if there are zeros, find max width bounding box
+            close_0 = det_0[torch.argmax((det_0[:,2] - det_0[:,0]), dim=0)] 
+            print("Det3 Width: ", close_0[2]-close_0[0])
+            if close_0[2]-close_0[0] > close_thresh:
+                return close_0
+            else:
+                if(det_no0.shape[0]>0):
+                    return det_no0[torch.argmin(det_no0[:,4], dim=0)]
+                else:
+                    return close_0            
+        else:
+            return det_no0[torch.argmin(det_no0[:,4], dim=0)]
+
+
         mogos.sort(key=lambda x:x[4])
         for mogo in mogos:
             if mogo[5] in colors:
@@ -36,7 +56,7 @@ def convert_rgb_to_hsv(r, g, b):
     return color_hsv
 
 def determine_color(det):
-    bgr = color_image0[int(det[1] + (float(det[3] - det[1]) * (2 / 10))):int(det[1] + (float(det[3] - det[1]) * (4.0 / 10))), int(det[0] + (float(det[2] - det[0]) * (4.0 / 10))):int(det[0] + (float(det[2] - det[0]) * (6.0 / 10)))]
+    bgr = color_image[int(det[1] + (float(det[3] - det[1]) * (2 / 10))):int(det[1] + (float(det[3] - det[1]) * (4.0 / 10))), int(det[0] + (float(det[2] - det[0]) * (4.0 / 10))):int(det[0] + (float(det[2] - det[0]) * (6.0 / 10)))]
     bgr = np.mean(bgr, axis=(0,1))
     hsv = convert_rgb_to_hsv(bgr[2],bgr[1],bgr[0])
     if hsv[0]>=180 and hsv[0]<=240:
@@ -45,7 +65,9 @@ def determine_color(det):
         return 0
     elif (hsv[0]>=0 and hsv[0]<20) or (hsv[0]>=320 and hsv[0]<=360):
         return -1
-    return 3
+    return 2
+    
+
 def determine_depth(det, do_depth_ring=False):
     if not do_depth_ring and not det[5] == 2:
         d = depth_image[int(det[1] + (float(det[3] - det[1]) * (2 / 10))):int(det[1] + (float(det[3] - det[1]) * (4.0 / 10))), int(det[0] + (float(det[2] - det[0]) * (4.0 / 10))):int(det[0] + (float(det[2] - det[0]) * (6.0 / 10)))]
@@ -55,6 +77,19 @@ def determine_depth(det, do_depth_ring=False):
         d = depth_image[int(det[1] + (float(det[3] - det[1]) * (2 / 10))):int(det[1] + (float(det[3] - det[1]) * (4.0 / 10))), int(det[0] + (float(det[2] - det[0]) * (4.0 / 10))):int(det[0] + (float(det[2] - det[0]) * (6.0 / 10)))]
         return np.mean(d)
     return -1
+
+def degree_test(det):
+    pixel_degree = 0.109375
+    center = 320
+    diff = center - (det[2] + det[0]) / 2
+    angle = diff*pixel_degree
+    print("AIF", angle)
+    print("DIF ", diff)
+
+def mindepth(pred):
+    return np.argmin(pred[:,4], axis=0)
+           
+    
         
 model = attempt_load(PATH)
 device = torch.device("cuda" if torch.cuda.is_available() else 'cpu')
@@ -81,7 +116,7 @@ else:
 # Start streaming
 pipeline.start(config)
 
-
+comm = Coms()
 try:
     while True:
         start = time.time()
@@ -110,12 +145,14 @@ try:
         color_image_t = torch.cuda.FloatTensor(color_image)
         color_image_t = torch.moveaxis(color_image_t, 2, 0)[None] / 255.0
         
+        print("CIT ", color_image_t.shape)
         pred = model(color_image_t)[0]
         conf_thres = .3
         pred = non_max_suppression(pred, conf_thres)
 
         color_image0, depth_colormap0 = color_image, depth_colormap
         pred = pred[0]
+        
         # tensor([[det1],[det2]])
         color_annotator = Annotator(color_image0, line_width=2, pil=not ascii)
         depth_annotator = Annotator(depth_colormap0, line_width=2, pil=not ascii)
@@ -127,13 +164,29 @@ try:
             if(det[5] == 0): # COLOR
                 pred[i, 5] = determine_color(det)
             else:
-                pred[i, 5] = 2
+                pred[i, 5] = 3
             pred[i, 4] = determine_depth(det, do_depth_ring=do_depth_ring) * depth_frame.get_units()
 
-            names = ["red-mogo","yellow-mogo", "blue-mogo","ring", "unknown_color"]
-            color_annotator.box_label(pred[i, :4], f'{names[int(pred[i, 5])+1]} {pred[i,4]:.2f}', color=colors(pred[i, 5], True))
-            depth_annotator.box_label(pred[i, :4], f'{names[int(pred[i, 5])+1]} {pred[i,4]:.2f}', color=colors(pred[i, 5], True))
+        names = ["red-mogo","yellow-mogo", "blue-mogo", "unknown_color", "ring"]
+        print(pred.shape)
+        if int(pred.shape[0])>0:
+            print(pred)
+            det = return_data(pred, find="close", colors=[-1,0,1])
 
+            if len(det)>0:
+                
+                color_annotator.box_label(det[:4], f'{names[int(det[5])+1]} {det[4]:.2f}', color=colors(det[5], True))
+                depth_annotator.box_label(det[:4], f'{names[int(det[5])+1]} {det[4]:.2f}', color=colors(det[5], True))
+                print((float((det[0] + det[2]) / 2) - 320) / 320)
+                comm.send("header", (float((det[0] + det[2]) / 2) - 320) / 320)
+            else:
+                comm.send("header", 0)
+        else:
+            comm.send("header", 0)
+        try:
+            print(comm.read())
+        except:
+            print("no data")
         color_image = color_annotator.result()
         depth_colormap = depth_annotator.result()
         
@@ -142,6 +195,8 @@ try:
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
         print(return_data(pred))
+        if len(pred) > 0:
+            degree_test(pred[0])
         print(time.time()-start)
         cv2.waitKey(1)
 
