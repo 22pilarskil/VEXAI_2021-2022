e514cee1c8d2b071db4e5322ef55b5ed48ae5f63
Liam <22pilarskil@millburn.org>
2021-11-23 20:01:14 -0500

    Refactor repo

--- python/yolo/yolo_vis.py
+++ python/yolo/yolo_vis.py
@@ -4,12 +4,13 @@ import numpy as np
 import cv2
 
 import torch
-from models.experimental import attempt_load
+from utils.experimental import attempt_load
 from utils.general import non_max_suppression, scale_coords
 from utils.plots import Annotator, colors
+from utils.serial_test import Coms
 import time
-import colorsys
-PATH = "/home/vexai/VEXAI_2021-2022/python/yolo/best.pt"
+
+PATH = os.getcwd() + "/models/best.pt"
 do_depth_ring = False
 
 import argparse
@@ -17,82 +18,6 @@ import argparse
 parser = argparse.ArgumentParser()
 parser.add_argument("--display", metavar="display", type=int, default=1)
 args = parser.parse_args()
-
-
-def return_data(mogos, find="all", colors=[-1,0,1], close_thresh=200):
-    # Takes in data in the order: [det:[x,y,x,y,dist,color (-1 = red, 0 = yellow, 1 = blue)], det[], .., det[]]
-    if find=="all":
-        if not colors == [-1,0,1]:
-            for i, mogo in enumerate(mogos):
-                if not mogo[5] in colors:
-                    del mogos[i]
-        return mogos
-    if find=="close":
-        mogos[mogos != mogos] = 0 #set all nans to 0
-        det_0 = mogos[mogos[:,4] == 0] #all zeros
-        det_no0 = mogos[mogos[:,4] != 0] #all non-zeros
-
-        if int(det_0.shape[0])>0:  #if there are zeros, find max width bounding box
-            close_0 = det_0[torch.argmax((det_0[:,2] - det_0[:,0]), dim=0)] 
-            print("Det3 Width: ", close_0[2]-close_0[0])
-            if close_0[2]-close_0[0] > close_thresh:
-                return close_0
-            else:
-                if(det_no0.shape[0]>0):
-                    return det_no0[torch.argmin(det_no0[:,4], dim=0)]
-                else:
-                    return close_0            
-        else:
-            return det_no0[torch.argmin(det_no0[:,4], dim=0)]
-
-
-        mogos.sort(key=lambda x:x[4])
-        for mogo in mogos:
-            if mogo[5] in colors:
-                return mogo
-
-def convert_rgb_to_hsv(r, g, b):
-    color_hsv_percentage=colorsys.rgb_to_hsv(r / float(255), g / float(255), b / float(255)) 
-    
-    color_h=round(360*color_hsv_percentage[0])
-    color_s=round(100*color_hsv_percentage[1])
-    color_v=round(100*color_hsv_percentage[2])
-    color_hsv=(color_h, color_s, color_v)
-    return color_hsv
-
-def determine_color(det):
-    bgr = color_image[int(det[1] + (float(det[3] - det[1]) * (2 / 10))):int(det[1] + (float(det[3] - det[1]) * (4.0 / 10))), int(det[0] + (float(det[2] - det[0]) * (4.0 / 10))):int(det[0] + (float(det[2] - det[0]) * (6.0 / 10)))]
-    bgr = np.mean(bgr, axis=(0,1))
-    hsv = convert_rgb_to_hsv(bgr[2],bgr[1],bgr[0])
-    if hsv[0]>=180 and hsv[0]<=240:
-        return 1
-    elif hsv[0]>=20 and hsv[0]<=100:
-        return 0
-    elif (hsv[0]>=0 and hsv[0]<20) or (hsv[0]>=320 and hsv[0]<=360):
-        return -1
-    return 2
-    
-
-def determine_depth(det, do_depth_ring=False):
-    if not do_depth_ring and not det[5] == 2:
-        d = depth_image[int(det[1] + (float(det[3] - det[1]) * (2 / 10))):int(det[1] + (float(det[3] - det[1]) * (4.0 / 10))), int(det[0] + (float(det[2] - det[0]) * (4.0 / 10))):int(det[0] + (float(det[2] - det[0]) * (6.0 / 10)))]
-        d = d[d>0]
-        return np.mean(d)
-    elif do_depth_ring:
-        d = depth_image[int(det[1] + (float(det[3] - det[1]) * (2 / 10))):int(det[1] + (float(det[3] - det[1]) * (4.0 / 10))), int(det[0] + (float(det[2] - det[0]) * (4.0 / 10))):int(det[0] + (float(det[2] - det[0]) * (6.0 / 10)))]
-        return np.mean(d)
-    return -1
-
-def degree(det):
-    pixel_degree = 0.109375
-    center = 320
-    diff = center - (det[2] + det[0]) / 2
-    angle = diff*pixel_degree
-    return angle
-
-def mindepth(pred):
-    return np.argmin(pred[:,4], axis=0)
-           
     
         
 model = attempt_load(PATH)
@@ -122,7 +47,6 @@ pipeline.start(config)
 
 comm = None
 try:
-    from serial_test import Coms
     comm = Coms()
 except:
     pass
@@ -154,8 +78,7 @@ try:
 
         color_image_t = torch.cuda.FloatTensor(color_image)
         color_image_t = torch.moveaxis(color_image_t, 2, 0)[None] / 255.0
-        
-        print("CIT ", color_image_t.shape)
+
         pred = model(color_image_t)[0]
         conf_thres = .3
         pred = non_max_suppression(pred, conf_thres)
@@ -167,8 +90,6 @@ try:
         color_annotator = Annotator(color_image0, line_width=2, pil=not ascii)
         depth_annotator = Annotator(depth_colormap0, line_width=2, pil=not ascii)
         pred[:,:4] = scale_coords(color_image_t.shape[2:], pred[:, :4], color_image0.shape).round()
-        color = []
-        depth = []
 
         for i, det in enumerate(pred):
             if(det[5] == 0): # COLOR
@@ -178,57 +99,41 @@ try:
             pred[i, 4] = determine_depth(det, do_depth_ring=do_depth_ring) * depth_frame.get_units()
 
         names = ["red-mogo","yellow-mogo", "blue-mogo", "unknown_color", "ring"]
-        print(pred.shape)
+
+        data = [0, 0]
   
         if int(pred.shape[0])>0:
-            print(pred)
             det = return_data(pred, find="close", colors=[-1,0,1])
 
             if len(det)>0:
                 
-                color_annotator.box_label(det[:4], f'{names[int(det[5])+1]} {det[4]:.2f}', color=colors(det[5], True))
-                depth_annotator.box_label(det[:4], f'{names[int(det[5])+1]} {det[4]:.2f}', color=colors(det[5], True))
+                if args.display:
+                    color_annotator.box_label(det[:4], f'{names[int(det[5])+1]} {det[4]:.2f}', color=colors(det[5], True))
+                    depth_annotator.box_label(det[:4], f'{names[int(det[5])+1]} {det[4]:.2f}', color=colors(det[5], True))
              
                 turn_angle = degree(det)
-                print("Turn angle {}".format(turn_angle))
-                print("Depth {}".format(det[4]))
+                print("Turn angle {}, Depth {}".format(turn_angle, det[4]))
                 if not turn_angle == None:
-                    try:
-                        comm.send("header", [float(det[4]), float(turn_angle)])
-                    except:
-                        try:
-                            print("Attempting reconnect")
-                            from serial_test import Coms
-                            comm = Coms()
-                            comm.send("header", [float(det[4]), float(turn_angle)])
-                            print("here")
-                        except Exception as e:
-                            print(e)
-                            continue
-    
-                else:
-                    comm.send("header", [0,0])
-            else:
-                comm.send("header", [0,0])
-        else:
-            comm.send("header", [0,0])
-
+                    data = [float(det[4]), float(turn_angle)]
+        
         try:
-            length = comm.read()
-            print("reading comm {}".format(length))
-            if (length > 1): 
-               print(comm.read())
-               while (comm.read() == 1): # TODO: This is stupid! We really shouldn't be stopping our camera when we hit a mogo, that's just silly. Maybe we do this in an actual match when the mogo obscures the camera, but for testing it really just causes the robot to die randomly.
+            comm.send("header", data)
+            if (comm.read("stop")): 
+               while (not comm.read("continue")): 
                  print("waiting")
         except:
-            print("no data")
-    color_image = color_annotator.result()
-    depth_colormap = depth_annotator.result()
-        
-    images = np.hstack((color_image, depth_colormap))
+            try:
+                comm = Coms()
+            except Exception as e:
+                print(e)
+
     if args.display:
+        color_image = color_annotator.result()
+        depth_colormap = depth_annotator.result()    
+        images = np.hstack((color_image, depth_colormap))
         cv2.namedWindow('RealSense', cv2.WINDOW_AUTOSIZE)
         cv2.imshow('RealSense', images)
+
     print(time.time()-start)
     cv2.waitKey(1)
 
