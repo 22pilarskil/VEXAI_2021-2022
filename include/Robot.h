e5bd20e99042524c26dd0e8817c0fd0f54a5510b
riyasinha06 <62569689+riyasinha06@users.noreply.github.com>
2022-04-06 10:05:29 -0400

    Begin updates to Robot.h for 15in

--- include/Robot.h
+++ include/Robot.h
@@ -17,31 +17,27 @@ class Robot{
 		static PD strafe_PD;
 		static PD turn_PD;
 
-		static Motor FLT;
-		static Motor FRT;
-		static Motor BLT;
-		static Motor BRT;
-		static Motor FLB;
-		static Motor FRB;
-		static Motor BLB;
-		static Motor BRB;
-		static Motor flicker;
-		static Motor angler;
-		static Motor conveyor;
-		static Motor lift;
+		static Motor FL;
+		static Motor ML;
+		static Motor BL;
+		static Motor FR;
+		static Motor MR;
+		static Motor BR;
+		static Motor four_bar;
+
 
 		static ADIEncoder LE;
 		static ADIEncoder RE;
 		static ADIEncoder BE;
-		static ADIAnalogIn angler_pot;
-		static ADIAnalogIn lift_pot;
-		static ADIDigitalOut angler_piston;
-		static ADIDigitalOut lift_piston;
-		static ADIUltrasonic ring_ultrasonic;
-		static Gps gps;
+		#static ADIAnalogIn angler_pot;
+		#static ADIAnalogIn lift_pot;
+		#static ADIDigitalOut angler_piston;
+		#static ADIDigitalOut lift_piston;
+		
+		#static Gps gps;
 		static Imu IMU;
-		static Distance angler_dist;
-		static Distance mogo_dist;
+		#static Distance angler_dist;
+		#static Distance mogo_dist;
 
 		static std::atomic<double> x;
 		static std::atomic<double> y;
@@ -49,7 +45,7 @@ class Robot{
 		static std::atomic<double> new_y;
 		static std::atomic<double> heading;
 		static std::atomic<double> imu_val;
-		static std::atomic<double> cur_x_gps;
+		/*static std::atomic<double> cur_x_gps;
 		static std::atomic<double> cur_y_gps;
 		static std::atomic<double> cur_pitch_gps;
 		static std::atomic<double> cur_roll_gps;
@@ -63,7 +59,7 @@ class Robot{
 		static std::atomic<double> last_y_gps_slow;
 		static std::atomic<double> new_x_gps;
         static std::atomic<double> new_y_gps;
-        static std::atomic<double> new_heading_gps;
+        static std::atomic<double> new_heading_gps;*/
         static std::atomic<bool> is_moving;
 		
 		static double offset_back;
